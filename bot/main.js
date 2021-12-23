32cd9c8ddf2ff3eee345a6acf0021c6321b805a0
eliasrilegard <e_rilegard@live.se>
2021-12-23 18:21:53 +0100

    Fixed dirname handling

--- bot/main.js
+++ bot/main.js
@@ -5,7 +5,7 @@ const config = require('../config.json');
 
 client = new Bot(/*config['bot']['defaultPrefix']*/);
 
-client.loadEvents(__dirname.replace('bot', 'events'))
-client.loadCommands(__dirname.replace('bot', 'commands'));
+client.loadEvents(__dirname.slice(0,-3).concat('events'));
+client.loadCommands(__dirname.slice(0,-3).concat('commands'));
 
 client.login(process.env.TOKEN)
