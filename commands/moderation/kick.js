f35e67de15b387415a6d1bddb3a11e9a74b1f619
eliasrilegard <e_rilegard@live.se>
2021-12-30 03:25:23 +0100

    Added way more checks before kick/ban is executed

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -6,24 +6,65 @@ class Kick extends Command {
     super('kick', 'Kick a member', '[member] [optional reason]', { guildOnly: true, permissions: 'KICK_MEMBERS' });
   }
 
-  async execute(message, args) {
+  async execute(message, args, client) {
     const member = message.mentions.members.first();
     const reason = args.slice(1).join(' ');
 
-    if (!member.kickable) {
-      const embed = new MessageEmbed()
-        .setColor('cc0000')
-        .setTitle('Cannot kick member')
+    const embed = new MessageEmbed().setColor('cc0000');
+
+    if (!member) {
+      embed
+        .setTitle('No user targeted')
+        .addField('Command usage', this.howTo(client.prefix(), true));
+      return message.channel.send({ embeds: [embed] });
+    }
+    if (member.id == message.author.id) {
+      embed
+        .setTitle('You can\'t kick yourself')
+        .setFooter({ text: 'Just leave 4Head' });
+      return message.channel.send({ embeds: [embed] });
+    }
+    if (member.id == client.user.id) {
+      embed.setTitle('I can\'t kick myself')
+      return message.channel.send({ embeds: [embed] });
+    }
+    if (client.isDev(member.id)) {
+      embed
+        .setTitle('You may not kick my dev')
+        .setFooter({ text: 'Don\'t even think about it >:c'});
+      return message.channel.send({ embeds: [embed] });
+    }
+    if (this.permHierarchy(member, message.member) && !message.member.permissions.has('ADMINISTRATOR')) {
+      embed
+        .setTitle('Can\'t kick member')
+        .setDescription('You can\'t kick someone equal to or above you');
+      return message.channel.send({ embeds: [embed] });
+    }
+    if (this.permHierarchy(member, message.guild.members.resolve(client.user))) {
+      embed
+        .setTitle('Can\'t kick member')
+        .setDescription('Specified user is above my highest role.');
+      return message.channel.send({ embeds: [embed] });
+    }
+    if (!member.kickable) { // Failsafe
+      embed
+        .setTitle('Can\'t kick member')
         .setDescription(`<@${member.user.id}> cannot be kicked.`)
       return message.channel.send({ embeds: [embed] });
     }
 
-    member.kick(reason);
-    const embed = new MessageEmbed()
+    member.kick({ reason: reason });
+
+    embed
       .setColor('00cc00')
       .setTitle('Success')
       .setDescription(`Successfully kicked <@${member.user.id}>${reason.length > 0 ? `for ${reason}` : ''}.`);
     message.channel.send({ embeds: [embed] });
+  }
+
+  permHierarchy(memberHigh, memberLow) {
+    // Returns true if memberHigh is higher or equal to memberLow in the role hierarchy
+    return memberLow.roles.highest.comparePositionTo(memberHigh.roles.highest) < 1;
   }
 }
 
