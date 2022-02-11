140f4ebe8e7d63a31b1297fe728f1cf61642a4cf
eliasrilegard <e_rilegard@live.se>
2022-02-11 16:07:37 +0100

    Reworked help command to utilize page handler

--- commands/general/help.ts
+++ commands/general/help.ts
@@ -1,28 +1,59 @@
-import { Message, MessageEmbed } from 'discord.js';
+import { Collection, Message, MessageEmbed } from 'discord.js';
 import Bot from '../../bot/bot';
 import Command from '../../bot/command';
 
 class Help extends Command {
   public constructor() {
-    super('help', 'List all commands or get info on a specific command.', '[command name]', { args: false, aliases: ['commands'] });
+    super(
+      'help',
+      'List all commands or get info on a specific command',
+      '(command)',
+      { args: false, aliases: ['commands'] }
+    );
   }
 
   public async execute(message: Message, args: Array<string>, client: Bot): Promise<void> {
     const prefix = await client.prefix(message);
-    const embed = new MessageEmbed().setColor('#0066cc');
 
-    if (!args.length) {
-      embed
-        .setTitle('Commands')
-        .addField('Here\'s a list of all avalible commands.', `\`${client.commands.filter(cmd => !cmd.devOnly).map(cmd => cmd.name).sort((a, b) => a.localeCompare(b)).join('\`, \`')}\``)
-        .setFooter({ text: `You can send ${this.howTo(prefix)} to get info on a specific command.` });
-      message.channel.send({ embeds: [embed] });
+    if (args.length === 0) {
+      const categories = new Array<[Command, Collection<string, Command>]>();
+      const categoryNames = [...client.categories.keys()];
+      categoryNames.forEach(name => categories.push([client.commands.get(name), client.categories.get(name)]));
+      
+      const categoryEmbeds = categories.map(category => {
+        const subCommands = [...category[1].entries()].map(commandEntry =>
+          `**${prefix}${category[0].name} ${commandEntry[0]}** - ${commandEntry[1].description}`
+        );
+        const categoryCommand = category[0];
+        const embed = new MessageEmbed()
+          .setColor('#0066cc')
+          .setTitle(categoryCommand.description)
+          .setDescription(categoryCommand.howTo(prefix))
+          .addField('Avalible subcommands', subCommands.join('\n\n'));
+        return embed;
+      });
+      
+      const categoriesOverview = categories.map(category => `**${prefix}${category[0].name}** - ${category[0].description}`).join('\n');
+      const standaloneCommands = client.commands
+        .filter(command => !command.category && !command.devOnly)
+        .map(command => `**${prefix}${command.name}** - ${command.description}`)
+        .sort((a, b) => a.localeCompare(b))
+        .join('\n');
+      
+      const mainPage = new MessageEmbed().setColor('#0066cc').setTitle('Avalible commands');
+      if (categoriesOverview.length) mainPage.addField('Category commands', categoriesOverview);
+      if (standaloneCommands.length) mainPage.addField('General commands', standaloneCommands);
+      mainPage.addField('Additional help', `You can send ${this.howTo(prefix, true)} to get additional info on a specific command.`);
+
+      this.sendMenu(message, [mainPage, ...categoryEmbeds]);
       return;
     }
 
     // Command has been specified
     const commandName = args[0].toLowerCase();
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+    const embed = new MessageEmbed();
 
     if (!command) {
       embed
@@ -33,27 +64,16 @@ class Help extends Command {
       return;
     }
 
-    embed.setTitle(`Command info: ${command.name}`);
+    embed.setColor('#0066cc').setTitle(`Command: ${command.name}`);
 
-    const desc = new Array();
-    const data = new Array();
+    if (command.aliases.length) embed.addField('Aliases', command.aliases.join(', '));
+    embed.addFields([
+      { name: 'Usage', value: command.howTo(prefix) },
+      { name: 'Description', value: command.description },
+      { name: 'Syntax', value: '[parameter] - Mandatory parameter\n(parameter) - Optional paramater' }
+    ]);
 
-    if (command.aliases.length) {
-      desc.push('**Aliases**');
-      data.push(`${command.aliases.join(', ')}`);
-    }
-    
-    desc.push('**Description**');
-    data.push(command.description);
 
-    desc.push('**Usage**');
-    data.push(command.howTo(prefix, true));
-
-    embed.addFields(
-      { name: '\u200b', value: desc.join('\n'), inline: true },
-      { name: '\u200b', value: data.join('\n'), inline: true }
-    );
-    
     message.channel.send({ embeds: [embed] });
   }
 }
