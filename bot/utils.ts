14821163a2d8c74fbb19dca23e1a567ecbdee9ef
eliasrilegard <e_rilegard@live.se>
2022-02-13 15:27:00 +0100

    Update shuffle to swap using array destructuring

--- bot/utils.ts
+++ bot/utils.ts
@@ -19,9 +19,7 @@ class Utils {
   public static shuffle(array: Array<unknown>): Array<unknown> {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+      [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
   }
