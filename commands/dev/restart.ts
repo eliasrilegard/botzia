b0d20fa14987de9bb01eb90debfd7058b791f773
eliasrilegard <e_rilegard@live.se>
2022-02-23 11:57:06 +0100

    Added restart command

---
+++ commands/dev/restart.ts
@@ -0,0 +1,27 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Bot from '../../bot/bot';
+import Command from '../../bot/command';
+
+class Restart extends Command {
+  public constructor() {
+    super(
+      'restart',
+      'Restarts the bot',
+      [''],
+      { args: false, devOnly: true }
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async execute(message: Message, _args: Array<string>, client: Bot): Promise<void> {
+    const embed = new MessageEmbed()
+      .setColor('#0066cc')
+      .setTitle('Restarting')
+      .setDescription('Sent request to respawn all shards.')
+      .setTimestamp();
+    await message.channel.send({ embeds: [embed] });
+    client.shard.respawnAll();
+  }
+}
+
+export default Restart;
