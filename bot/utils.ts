a71ccbe50818c082a35a4120b8af0ee77d5c166f
eliasrilegard <e_rilegard@live.se>
2022-03-01 14:02:12 +0100

    Added basic documentation for utility functions

--- bot/utils.ts
+++ bot/utils.ts
@@ -3,15 +3,24 @@ import { GuildMember } from 'discord.js';
 // A utility class with common functions required by some modules to run.
 
 class Utils {
+  /**
+   * Capitalizes the first letter of a given string.
+   */
   public static capitalize(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
+  /**
+   * Compares the highest permissions of two given members.
+   * @returns `true` if memberHigh is higher or equal to memberLow in the role hierarchy.
+   */
   public static permHierarchy(memberHigh: GuildMember, memberLow: GuildMember): boolean {
-    // Returns true if memberHigh is higher or equal to memberLow in the role hierarchy
     return memberLow.roles.highest.comparePositionTo(memberHigh.roles.highest) < 1;
   }
 
+  /**
+   * Shuffles the passed array.
+   */
   public static shuffle<T>(array: Array<T>): Array<T> {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
