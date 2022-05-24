e8e0ae7ea1413c1e7163d4cb4044ca713f995af0
eliasrilegard <e_rilegard@live.se>
2022-05-24 14:05:26 +0200

    Simple database utility command

---
+++ commands/dev/db.ts
@@ -0,0 +1,41 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Bot from '../../bot/bot';
+import Command from '../../bot/command';
+
+class Db extends Command {
+  constructor() {
+    super(
+      'db',
+      'Database management commands',
+      ['[command]'],
+      { devOnly: true }
+    );
+  }
+
+  public async execute(message: Message, args: Array<string>, client: Bot): Promise<void> {
+    switch (args[0]) {
+      case 'reload': {
+        client.apiClient.serverPrefixes.loadDatabase();
+        client.apiClient.userTimezones.loadDatabase();
+
+        const embed = new MessageEmbed()
+          .setColor('#00cc00')
+          .setTitle('Database reloaded')
+          .setDescription('Successfully reloaded all databases.');
+        message.channel.send({ embeds: [embed] });
+        return;
+      }
+
+      default: {
+        const embed = new MessageEmbed()
+          .setColor('#cc0000')
+          .setTitle('No such command')
+          .setDescription(`The command \`${args[0]}\` couldn't be found.`);
+        message.channel.send({ embeds: [embed] });
+        return;
+      }
+    }
+  }
+}
+
+export default Db;
