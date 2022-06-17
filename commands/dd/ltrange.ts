fa00a76da8068b384625e18f186f42bccfc263f3
eliasrilegard <e_rilegard@live.se>
2022-06-17 21:26:16 +0200

    Command to view closest breakpoints to current tower range

---
+++ commands/dd/ltrange.ts
@@ -0,0 +1,65 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Bot from '../../bot/bot';
+import Command from '../../bot/command';
+
+class LTRange extends Command {
+  // These offsets are taken from https://quarplet.com/chaintargets_breakpoints.txt
+  // Since we're storing the breakpoints in an array, we need to offset the index by some value
+  private normal: Array<number>; // +6
+  private upped: Array<number>; // +17
+
+  public constructor() {
+    super(
+      'ltrange',
+      'Finds the closest LT breakpoints to your current range',
+      ['[current range]'],
+      { belongsTo: 'dd', devOnly: true }
+    );
+
+    this.normal = [0, 1, 2, 4, 6, 8, 11, 14, 19, 23, 29, 35, 43, 51, 60, 70, 81, 93, 106, 120, 136, 153, 171, 190, 211, 233, 257, 282, 309, 337, 367, 399, 432, 468, 504, 543, 584, 626, 671, 717, 766, 816, 869, 924, 981, 1040, 1101, 1165, 1231, 1300, 1370, 1444, 1519, 1598, 1679, 1762, 1848, 1937, 2028, 2122, 2219, 2319, 2421, 2526, 2635, 2746, 2860, 2977, 3098, 3221, 3347, 3477, 3610, 3746, 3885, 4027, 4173, 4322, 4475, 4631, 4790, 4953, 5119, 5289, 5463, 5640, 5820, 6005, 6193, 6385, 6581, 6780, 6983, 7190, 7402, 7617, 7835, 8058, 8285, 8516, 8752, 8991, 9234, 9482, 9734, 9990];
+    this.upped = [0, 7, 15, 24, 34, 45, 57, 70, 84, 100, 117, 135, 154, 175, 197, 221, 246, 273, 301, 331, 363, 396, 432, 468, 507, 548, 590, 635, 681, 730, 780, 833, 888, 945, 1004, 1065, 1129, 1195, 1264, 1334, 1408, 1483, 1562, 1643, 1726, 1812, 1901, 1992, 2086, 2183, 2283, 2385, 2490, 2599, 2710, 2824, 2941, 3062, 3185, 3311, 3441, 3574, 3710, 3849, 3991, 4137, 4286, 4439, 4595, 4754, 4917, 5083, 5253, 5427, 5604, 5784, 5969, 6157, 6349, 6545, 6744, 6947, 7154, 7366, 7581, 7799, 8022, 8249, 8480, 8716, 8955, 9198, 9446, 9698, 9954];
+  }
+
+  public async execute(message: Message, args: Array<string>, client: Bot): Promise<void> {
+    if (args.length > 1) {
+      const embed = new MessageEmbed()
+        .setColor('#cc0000')
+        .setTitle('Check arguments')
+        .setDescription('This command takes 1 argument.')
+        .addField('Usage', this.howTo(await client.prefix(message), true));
+      message.channel.send({ embeds: [embed] });
+      return;
+    }
+
+    const currentRange = parseInt(args[0]);
+    if (isNaN(currentRange)) {
+      const embed = new MessageEmbed()
+        .setColor('#cc0000')
+        .setTitle('Check arguments')
+        .setDescription('This command takes a number as its argument.')
+        .addField('Usage', this.howTo(await client.prefix(message), true));
+      message.channel.send({ embeds: [embed] });
+      return;
+    }
+
+    // Find closest 3 breakpoints in normal and upped
+    const indexNormal = this.normal.findIndex(x => x >= currentRange);
+    const indexUpped = this.upped.findIndex(x => x >= currentRange);
+
+    // Construct the embed content by showing the 3 closest breakpoints and their values
+    const valueNormal = `\`\`\`${this.normal[indexNormal - 1]} - ${indexNormal + 5}\n${this.normal[indexNormal]} - ${indexNormal + 6}\n${this.normal[indexNormal + 1]} - ${indexNormal + 7}\`\`\``;
+    const valueUpped = `\`\`\`${this.upped[indexUpped - 1]} - ${indexUpped + 16}\n${this.upped[indexUpped]} - ${indexUpped + 17}\n${this.upped[indexUpped + 1]} - ${indexUpped + 18}\`\`\``;
+
+    const embed = new MessageEmbed()
+      .setColor('#0066cc')
+      .setTitle('Closest breakpoints')
+      .setDescription('Here are the closest breakpoints to your current range.\n\`Range - Targets\`')
+      .addFields(
+        { name: 'Normal', value: valueNormal, inline: true },
+        { name: 'Upgraded', value: valueUpped, inline: true }
+      );
+    message.channel.send({ embeds: [embed] });
+  }
+}
+
+export default LTRange;
