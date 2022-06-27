a529f1d89e436d4e3cabf9b45576fb39c1a1efa7
eliasrilegard <e_rilegard@live.se>
2022-06-27 12:51:00 +0200

    Specify type for config

--- bot/main.ts
+++ bot/main.ts
@@ -1,6 +1,6 @@
-import Bot from './bot';
+import Bot, { ClientConfig } from './bot';
 import * as config from '../config.json';
 
-const client = new Bot(__dirname, config);
+const client = new Bot(__dirname, config as ClientConfig);
 
 client.login(process.env.TOKEN);
