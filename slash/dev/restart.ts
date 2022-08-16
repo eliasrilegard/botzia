b601d1bf49061c83c30ce4577e67ed145c4c2712
eliasrilegard <e_rilegard@live.se>
2022-08-16 23:14:33 +0200

    Port restart to slash command

---
+++ slash/dev/restart.ts
@@ -0,0 +1,22 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandSubcommandBuilder } from 'discord.js';
+import Bot from '../../bot/bot';
+import SlashCommand from '../../bot/slashcommand';
+
+export default class Restart extends SlashCommand {
+  constructor(client?: Bot) {
+    const data = new SlashCommandSubcommandBuilder()
+      .setName('restart')
+      .setDescription('Restart the bot');
+    super(data, client, { belongsTo: 'dev' });
+  }
+
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const embed = new EmbedBuilder()
+      .setColor(this.client.config.colors.GREEN)
+      .setTitle('Restarting')
+      .setDescription('Sent request to respawn all shards.')
+      .setTimestamp();
+    await interaction.reply({ embeds: [embed] });
+    this.client.shard.respawnAll();
+  }
+}
