bbcbe941a0f4f5ab2a6be8eb396782dbe88987a2
eliasrilegard <e_rilegard@live.se>
2022-10-01 16:50:16 +0200

    Refactor and add autocomplete support

--- slash/dd/quote.ts
+++ slash/dd/quote.ts
@@ -1,8 +1,10 @@
-import { AttachmentBuilder, ChatInputCommandInteraction, SlashCommandSubcommandBuilder } from 'discord.js';
+import { AttachmentBuilder, AutocompleteInteraction, ChatInputCommandInteraction, EmbedBuilder, SlashCommandSubcommandBuilder } from 'discord.js';
 import Bot from '../../bot/bot';
 import SlashCommand from '../../bot/slashcommand';
 
 export default class Quote extends SlashCommand {
+  private readonly quoteMap: Map<string, string>;
+
   constructor(client: Bot) {
     const data = new SlashCommandSubcommandBuilder()
       .setName('quote')
@@ -10,28 +12,50 @@ export default class Quote extends SlashCommand {
       .addStringOption(option => option
         .setName('quote')
         .setDescription('The quote to post')
-        .addChoices(
-          { name: 'cheating', value: 'plane_cheating' },
-          { name: 'faking', value: 'thales_faking_traces' },
-          { name: 'possible', value: 'lolz_possible' },
-          { name: 'post', value: 'nyman_post' },
-          { name: 'trash', value: 'jeol_trash' },
-          { name: 'ups', value: 'moose_ups' },
-          { name: 'verylucky', value: 'washboard_tol' }
-        )
+        .setAutocomplete(true)
         .setRequired(true)
       );
     super(data, client, { belongsTo: 'dd' });
+
+    this.quoteMap = new Map();
+    this.loadQuotes();
   }
 
   async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-    const key = interaction.options.getString('quote');
-    const filepath = `./database/dungeon_defenders/quotes/img/${key}.png`;
+    const key = interaction.options.getString('quote')!;
+    const filepath = this.quoteMap.get(key);
+
+    if (!filepath) {
+      const embed = new EmbedBuilder()
+        .setColor(this.client.config.colors.RED)
+        .setTitle('Quote not found')
+        .setDescription('Could not identify the quote.');
+      interaction.reply({ embeds: [embed] });
+      return;
+    }
     
     const quoteImage = new AttachmentBuilder(
       filepath,
       { name: filepath.slice(filepath.lastIndexOf('/') + 1).replace(/[',\s-]/g, '') } // See mhw/hzv
     );
     interaction.reply({ files: [quoteImage] });
+  }
+
+  async handleAutocomplete(interaction: AutocompleteInteraction): Promise<void> {
+    const focusedValue = interaction.options.getFocused().toLowerCase().replace(/\s+/g, '');
+    const options: Array<{ name: string, value: string }> = [];
+    for (const key of this.quoteMap.keys()) {
+      if (key && key.includes(focusedValue)) options.push({ name: key, value: key });
+    }
+    await interaction.respond(options.length < 26 ? options : []);
+  }
+
+  private async loadQuotes(): Promise<void> {
+    delete require.cache[require.resolve('../../database/dungeon_defenders/quotes/quotemap.json')];
+    const rawImport = await import('../../database/dungeon_defenders/quotes/quotemap.json');
+    const quoteData = rawImport.slice(0, -1); // Last element has weird shit in it
+    for (const [, v] of Object.entries(quoteData)) {
+      this.quoteMap.set(v.name, v.filepath);
+    }
   }
 }
