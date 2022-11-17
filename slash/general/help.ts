cdd43ec6a0d25d5892dda62b95a874858cb3075e
eliasrilegard <e_rilegard@live.se>
2022-11-17 16:09:06 +0100

    Initial version of help command, ported from text command

---
+++ slash/general/help.ts
@@ -0,0 +1,42 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import Bot from '../../bot/bot';
+import SlashCommand from '../../bot/slashcommand';
+
+export default class Help extends SlashCommand {
+  constructor(client: Bot) {
+    const data = new SlashCommandBuilder()
+      .setName('help')
+      .setDescription('List all available commands or get info on a specific command');
+    super(data as SlashCommandBuilder, client);
+  }
+
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const categories = this.client.slashCommands.filter(cmd => (cmd.isCategory ?? false) && cmd.data.name !== 'dev');
+
+    const categoryEmbeds = categories.map(categoryCmd => {
+      const subCommands = this.client.slashCommands
+        .filter(cmd => cmd.belongsTo === categoryCmd.data.name)
+        .map(cmd => `**/${categoryCmd.data.name} ${cmd.data.name}** - ${cmd.data.description}`);
+      const embed = new EmbedBuilder()
+        .setColor(this.client.config.colors.BLUE)
+        .setTitle(categoryCmd.data.description)
+        .addFields({ name: 'Available subcommands', value: subCommands.join('\n\n') });
+      return embed;
+    });
+
+    const categoriesOverview = categories.map(categoryCmd => `**/${categoryCmd.data.name}** - ${categoryCmd.data.description}`).join('\n');
+    const standaloneCommands = this.client.slashCommands
+      .filter(cmd => !cmd.isCategory && !cmd.belongsTo)
+      .map(cmd => `**/${cmd.data.name}** - ${cmd.data.description}`)
+      .sort((a, b) => a.localeCompare(b))
+      .join('\n');
+
+    const mainPage = new EmbedBuilder()
+      .setColor(this.client.config.colors.BLUE)
+      .setTitle('Available commands');
+    if (categoriesOverview.length) mainPage.addFields({ name: 'Category commands', value: categoriesOverview });
+    if (standaloneCommands.length) mainPage.addFields({ name: 'General commands', value: standaloneCommands });
+
+    this.sendMenu(interaction, [mainPage, ...categoryEmbeds]);
+  }
+}
