cf39726272f56cc4d4c31c665ca1209a4af8d62a
eliasrilegard <e_rilegard@live.se>
2023-07-23 12:30:41 +0930

    Add purge command
    Some paths still untested

--- src/commands/moderation/mod.rs
+++ src/commands/moderation/mod.rs
@@ -3,11 +3,13 @@ use super::NamedCommands;
 mod ban;
 mod massmove;
 mod kick;
+mod purge;
 
 pub fn commands() -> NamedCommands {
   vec![
     ("ban", Box::<ban::Ban>::default()),
     ("massmove", Box::<massmove::MassMove>::default()),
-    ("kick", Box::<kick::Kick>::default())
+    ("kick", Box::<kick::Kick>::default()),
+    ("purge", Box::<purge::Purge>::default())
   ]
 }
