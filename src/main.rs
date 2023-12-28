e69425cb52c92ba58e6090314183ef19dfb65504
eliasrilegard <e_rilegard@live.se>
2023-12-28 10:43:58 +0100

    Add missing migration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use anyhow::Context;
 use serenity::all::GatewayIntents;
 use serenity::Client;
 use shuttle_secrets::{SecretStore, Secrets};
 use shuttle_serenity::ShuttleSerenity;
 use shuttle_shared_db::Postgres;
-use sqlx::PgPool;
+use sqlx::{Executor, PgPool};
 
 mod color;
 mod commands;
@@ -18,8 +19,13 @@ pub type Result<T, E = Box<dyn std::error::Error + Send + Sync>> = std::result::
 
 #[shuttle_runtime::main]
 async fn serenity(#[Postgres] pool: PgPool, #[Secrets] secret_store: SecretStore) -> ShuttleSerenity {
-  let token = secret_store.get("DISCORD_TOKEN").expect("DISCORD_TOKEN not found");
+  let token = secret_store.get("DISCORD_TOKEN").context("DISCORD_TOKEN not found")?;
   let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MEMBERS;
+
+  pool
+    .execute(include_str!("../schema.sql"))
+    .await
+    .context("Failed to run migrations")?;
 
   let db = Database::new(pool);
   let _ = db.watch_reminders(&token);
@@ -29,6 +35,6 @@ async fn serenity(#[Postgres] pool: PgPool, #[Secrets] secret_store: SecretStore
   let client = Client::builder(token, intents)
     .event_handler(handler)
     .await
-    .expect("Error creating client");
+    .context("Error creating client")?;
   Ok(client.into())
 }
